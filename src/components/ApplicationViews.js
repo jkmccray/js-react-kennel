f9b90b9db388774bd0afef1dbb81466ceaee744a
Jacquelyn M <jacquelynkmccray@gmail.com>
2019-08-26 13:56:50 -0500

    completed ch 5 and 6 exercises

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -2,8 +2,9 @@ import { Route } from 'react-router-dom'
 import React, { Component } from 'react'
 import Home from './home/Home'
 import AnimalList from './animal/AnimalList'
-//only include these once they are built - previous practice exercise
+import AnimalDetail from './animal/AnimalDetail'
 import LocationList from './location/LocationList'
+import LocationDetail from './location/LocationDetail'
 import EmployeeList from './employee/EmployeeList'
 import OwnerList from './owner/OwnerList'
 
@@ -16,11 +17,27 @@ class ApplicationViews extends Component {
         <Route exact path="/" render={(props) => {
           return <Home />
         }} />
-        <Route path="/animals" render={(props) => {
+        <Route exact path="/animals" render={(props) => {
           return <AnimalList />
         }} />
-        <Route path="/locations" render={(props) => {
+        <Route path="/animals/:animalId(\d+)" render={(props) => {
+          // Pass the animalId to the AnimalDetailComponent
+          return <AnimalDetail animalId={parseInt(props.match.params.animalId)} />
+        }} />
+        {/*
+          This is a new route to handle a URL with the following pattern:
+          http://localhost:3000/animals/1
+
+          It will not handle the following URL because the `(\d+)`
+          matches only numbers after the final slash in the URL
+          http://localhost:3000/animals/jack
+        */}
+        <Route exact path="/locations" render={(props) => {
           return <LocationList />
+        }} />
+        <Route path="/locations/:locationId(\d+)" render={(props) => {
+          // Pass the locationId to the LocationDetailComponent
+          return <LocationDetail locationId={parseInt(props.match.params.locationId)} />
         }} />
         <Route path="/employees" render={(props) => {
           return <EmployeeList />
--- src/components/location/LocationCard.js
+++ src/components/location/LocationCard.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from "react-router-dom";
 
 class LocationCard extends Component {
   render() {
@@ -7,6 +8,7 @@ class LocationCard extends Component {
         <div className="card-content">
           <h3>Name: <span className="card-locationname">{this.props.location.name}</span></h3>
           <address>{this.props.location.address}</address>
+          <Link to={`/locations/${this.props.location.id}`}><button>Details</button></Link>
           <button type="button" onClick={() => this.props.deleteLocation(this.props.location.id)}>Close Location</button>
         </div>
       </div>
